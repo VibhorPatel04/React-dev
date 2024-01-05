dd27721cfb842dc31aed941d8072e0eba9f84e88
Vibhor Patel <vibhor.patrl04@gmail.com>
2024-01-05 10:40:53 +0530

    get data form Array

--- App.js
+++ App.js
@@ -9,16 +9,19 @@ import ReactDOM from "react-dom/client";
  * - RastaurentContainer
  *   - RestaurentCard
  *     -  img, res name, rating, cuisine, delivery time
- * Footer 
+ * Footer
  * - Copyright
  * - Links
  * - Contact
-**/
+ **/
 const Header = () => {
-  return(
+  return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
       </div>
       <div className="nav-Items">
         <ul>
@@ -29,132 +32,451 @@ const Header = () => {
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 // const StyleCard = {
 //   backgroundColor: "#f0f0f0"
 // }
 const RestaurentCard = (props) => {
-  const {resData} = props;
-  return(
-    <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
-      <img className="res-img" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+(resData.info.cloudinaryImageId)} />
-      <h3>{resData.info.name}</h3>
-      <h4>{resData.info.locality}</h4>
-      <h4>{resData.info.avgRating}</h4>
-      <h4>{resData.info.sla.deliveryTime} min</h4>
-      <h4>{resData.info.costForTwo}</h4>
+  const { resData } = props;
+  const {cloudinaryImageId, name, cuisines, avgRating,deliveryTime, costForTwo} = resData?.info;
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-img"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}*</h4>
+      {/* <h4>{deliveryTime} min</h4> */}
+      <h4>{costForTwo}</h4>
     </div>
-  )
-}
+  );
+};
 
-const resObj =   {
-  "info": {
-      "id": "30199",
-      "name": "Shri Ram Corner Paharghanj Ke Mashoor Choley Bhature (Shalimar Bagh)",
-      "cloudinaryImageId": "rb091alr5ye61q4yocsd",
-      "locality": "Shalimar Bagh",
-      "areaName": "Ashok vihar",
-      "costForTwo": "₹150 for two",
-      "cuisines": [
-          "Street Food"
-      ],
-      "avgRating": 4.6,
-      "veg": true,
-      "parentId": "16671",
-      "avgRatingString": "4.6",
-      "totalRatingsString": "5K+",
-      "sla": {
-          "deliveryTime": 31,
-          "lastMileTravel": 7.7,
-          "serviceability": "SERVICEABLE",
-          "slaString": "31 mins",
-          "lastMileTravelString": "7.7 km",
-          "iconType": "ICON_TYPE_EMPTY"
+const resList = [
+  {
+    info: {
+      id: "30199",
+      name: "Shri Ram Corner Paharghanj Ke Mashoor Choley Bhature (Shalimar Bagh)",
+      cloudinaryImageId: "rb091alr5ye61q4yocsd",
+      locality: "Shalimar Bagh",
+      areaName: "Ashok vihar",
+      costForTwo: "₹150 for two",
+      cuisines: ["Street Food"],
+      avgRating: 4.6,
+      veg: true,
+      parentId: "16671",
+      avgRatingString: "4.6",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 7.7,
+        serviceability: "SERVICEABLE",
+        slaString: "31 mins",
+        lastMileTravelString: "7.7 km",
+        iconType: "ICON_TYPE_EMPTY",
       },
-      "availability": {
-          "nextCloseTime": "2024-01-04 15:45:00",
-          "opened": true
+      availability: {
+        nextCloseTime: "2024-01-04 15:45:00",
+        opened: true,
       },
-      "badges": {
-          "imageBadges": [
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
               {
-                  "imageId": "v1695133679/badges/Pure_Veg111.png",
-                  "description": "pureveg"
-              }
-          ]
-      },
-      "isOpen": true,
-      "type": "F",
-      "badgesV2": {
-          "entityBadges": {
-              "imageBased": {
-                  "badgeObject": [
-                      {
-                          "attributes": {
-                              "description": "pureveg",
-                              "imageId": "v1695133679/badges/Pure_Veg111.png"
-                          }
-                      }
-                  ]
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
               },
-              "textBased": {},
-              "textExtendedBadges": {}
-          }
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
       },
-      "aggregatedDiscountInfoV3": {
-          "header": "₹120 OFF",
-          "subHeader": "ABOVE ₹199",
-          "discountTag": "FLAT DEAL"
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
       },
-      "orderabilityCommunication": {
-          "title": {},
-          "subTitle": {},
-          "message": {},
-          "customIcon": {}
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
       },
-      "differentiatedUi": {
-          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-          "differentiatedUiMediaDetails": {
-              "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-              "lottie": {},
-              "video": {}
-          }
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
       },
-      "reviewsSummary": {},
-      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-      "restaurantOfferPresentationInfo": {}
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
   },
-  "analytics": {
-      "context": "seo-data-da77b5af-def2-49ed-98b8-41a29662cb57"
+  {
+    info: {
+      id: "15846",
+      name: "Faasos - Wraps, Rolls & Shawarma",
+      cloudinaryImageId: "af33b81798b11deba338e94b7585d348",
+      locality: "Sector 8",
+      areaName: "Rohini",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "Kebabs",
+        "Fast Food",
+        "Snacks",
+        "American",
+        "Healthy Food",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 4.3,
+      parentId: "21809",
+      avgRatingString: "4.3",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 26,
+        lastMileTravel: 2.7,
+        serviceability: "SERVICEABLE",
+        slaString: "26 mins",
+        lastMileTravelString: "2.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-04 23:59:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹129",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
   },
-  "cta": {
-      "link": "https://www.swiggy.com/restaurants/shri-ram-corner-paharghanj-ke-mashoor-choley-bhature-shalimar-bagh-shalimar-bagh-ashok-vihar-delhi-30199",
-      "text": "RESTAURANT_MENU",
-      "type": "WEBLINK"
+  {
+    info: {
+      id: "62876",
+      name: "LunchBox - Meals and Thalis",
+      cloudinaryImageId: "dr7zk0fge6mhr6xtsfat",
+      locality: "Sector 8",
+      areaName: "Rohini",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "Biryani",
+        "North Indian",
+        "Punjabi",
+        "Healthy Food",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 4.2,
+      parentId: "4925",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 2.7,
+        serviceability: "SERVICEABLE",
+        slaString: "27 mins",
+        lastMileTravelString: "2.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-04 23:59:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹159",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
   },
-  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
-}
+  {
+    info: {
+      id: "239435",
+      name: "La Pino'z Pizza",
+      cloudinaryImageId: "tolejbnae10pq0udbavn",
+      locality: "New delhi",
+      areaName: "Paschim Vihar",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas", "Pastas", "Italian", "Desserts", "Beverages"],
+      avgRating: 4.1,
+      parentId: "4961",
+      avgRatingString: "4.1",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 32,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "32 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-05 05:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+  },
+  {
+    info: {
+      id: "16866",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Sector 3",
+      areaName: "Rohini",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4,
+      parentId: "721",
+      avgRatingString: "4.0",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 29,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "29 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-05 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+  },
+  {
+    info: {
+      id: "341033",
+      name: "Bakingo",
+      cloudinaryImageId: "0c53fd8f62e3d25ba56d1b4cfe572cef",
+      locality: "Rithala",
+      areaName: "Rohini",
+      costForTwo: "₹300 for two",
+      cuisines: ["Bakery", "Desserts", "Beverages", "Snacks"],
+      avgRating: 4.4,
+      parentId: "3818",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 22,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "22 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-05 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹120 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+  },
+];
 
 const Body = () => {
-  return(
+  return (
     <div className="body">
       <div className="search">Search</div>
       <div className="restaurent-container">
-        <RestaurentCard resData={resObj} />
+       {
+        resList.map((restaurent)=>{
+          return  <RestaurentCard key={restaurent.info.id} resData={restaurent}/>;
+        })
+        } 
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
 const AppLayout = () => {
-  return(
+  return (
     <div className="app">
       <Header />
       <Body />
     </div>
-  )
-}
+  );
+};
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
