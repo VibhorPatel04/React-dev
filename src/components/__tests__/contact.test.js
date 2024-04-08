90d5fe32f2584b1e982728cea4d87a7bba0a21fc
Vibhor Patel <vibhor.patrl04@gmail.com>
2024-04-08 23:04:19 +0530

    this is called unit testing

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -2,29 +2,27 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-describe("Contact us page test cases", () =>{
-    test("Should load contact us component", () =>{
-        render(<Contact />)
-    
-        const heading = screen.getByRole("heading");
-        // Assertion
-        expect(heading).toBeInTheDocument();
-    });
-    test("should load input name inside contact us component", () =>{
-        render(<Contact />);
-    
-        const lable = screen.getByPlaceholderText("name");
-        expect(lable).toBeInTheDocument();
-    });
-    test("should load 2 input box inside contact us component", ()=>{
-        render(<Contact />);
-    
-        const inputBoxs = screen.getAllByRole("textbox");
-        console.log(inputBoxs.length);
-        //Assertion 
-    
-        expect(inputBoxs.length).toBe(3);
-    });
+describe("Contact us page test cases", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
 
+    const heading = screen.getByRole("heading");
+    // Assertion
+    expect(heading).toBeInTheDocument();
+  });
+  test("should load input name inside contact us component", () => {
+    render(<Contact />);
+
+    const lable = screen.getByPlaceholderText("name");
+    expect(lable).toBeInTheDocument();
+  });
+  test("should load 2 input box inside contact us component", () => {
+    render(<Contact />);
+
+    const inputBoxs = screen.getAllByRole("textbox");
+    console.log(inputBoxs.length);
+    //Assertion
+
+    expect(inputBoxs.length).toBe(3);
+  });
 });
-
